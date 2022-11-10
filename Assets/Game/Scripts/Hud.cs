84c4244e7b9c412f90d56fbd596ebe02b363a8f1
evgenbaranenko <evgenbaranenko@gmail.com>
2022-11-10 13:10:30 +0200

    fix all bags, end Practical Task 21

--- Assets/Game/Scripts/Hud.cs
+++ Assets/Game/Scripts/Hud.cs
@@ -10,21 +10,32 @@ public class Hud : MonoBehaviour
     [SerializeField] private Text m_turnsValue;
 
     private static Hud m_instance;
-    public static Hud Instance { get { return m_instance; } }   
+    public static Hud Instance { get { return m_instance; } }
+
+    [SerializeField] private Slider m_musicSlider;
+
+    [SerializeField] private Slider m_soundSlider;
 
     private void Awake()
     {
         m_instance = this;
     }
+    public void UpdateOptions()
+    {
+        m_musicSlider.value = Controller.Instance.Audio.MusicVolume;
+
+        m_soundSlider.value = Controller.Instance.Audio.SfxVolume;
+    }
+
     public void UpdateTurnsValue(int value)
     {
         m_turnsValue.text = value.ToString();
     }
-
     public void UpdateScoreValue(int value)
     {
         m_scoreValue.text = value.ToString();
     }
+
     public void ShowWindow(CanvasGroup window)
     {
         window.alpha = 1f;
@@ -48,5 +59,14 @@ public class Hud : MonoBehaviour
     public void Reset()
     {
         Controller.Instance.Reset();
+    }
+    //методи, які будуть задавати гучність звуків та музики:
+    public void SetMusicVolume(float volume)
+    {
+        Controller.Instance.Audio.MusicVolume = volume;
+    }
+    public void SetSoundVolume(float volume)
+    {
+        Controller.Instance.Audio.SfxVolume = volume;
     }
 }
